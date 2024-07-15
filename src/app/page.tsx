5da439e69c9b5dd9f8466f5e82b5b8d751ed64f2
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 18:26:29 +0330

    connect Home page with backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { FaBars, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 export default function Home() {
+  const { status } = useSession();
+
   return (
     <>
       <div className="container">
@@ -9,42 +14,45 @@ export default function Home() {
           <h3 className="sidebar-title">Sidebar</h3>
 
           <ul className="sidebar-links">
-            <>
-              <li>
-                <Link href="/dashboard">
-                  <span>
-                    <FaBars />
-                  </span>
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link href="#">
-                  <span>
-                    <FaSignOutAlt />
-                  </span>
-                  Logout
-                </Link>
-              </li>
-            </>
-            <>
-              <li>
-                <Link href="/signin">
-                  <span>
-                    <FaSignInAlt />
-                  </span>
-                  Sign in
-                </Link>
-              </li>
-              <li>
-                <Link href="/signup">
-                  <span>
-                    <FaSignInAlt />
-                  </span>
-                  Sign up
-                </Link>
-              </li>
-            </>
+            {status === "authenticated" ? (
+              <>
+                <li>
+                  <Link href="/dashboard">
+                    <span>
+                      <FaBars />
+                    </span>
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#" onClick={() => signOut({ redirect: false })}>
+                    <span>
+                      <FaSignOutAlt />
+                    </span>
+                    Logout
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href="/signin">
+                    <span>
+                      <FaSignInAlt />
+                    </span>
+                    Sign in
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/signup">
+                    <span>
+                      <FaSignInAlt />
+                    </span>
+                    Sign up
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
           <img className="wave" src="/Images/wave.svg" alt="wave" />
         </aside>
