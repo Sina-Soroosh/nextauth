ea6a6f48612fe2ce006fd307b0c219ef0bdda834
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 15:45:47 +0330

    coding dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <h1 className="text-xl m-4 text-white">
+        Welcome to Dashboard , Sina - Soroosh{" "}
+      </h1>
+    </>
+  );
+}
+
+export default page;
