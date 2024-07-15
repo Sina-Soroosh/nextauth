d0cd4972aceb1130fe687827693a9e06a90dfe5a
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 16:17:57 +0330

    coding User model

---
+++ src/app/models/User.ts
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+export default model;
