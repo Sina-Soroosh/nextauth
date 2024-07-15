b33c26a9d0b4bdda364d40908c7f5737f768610e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 16:03:15 +0330

    coding signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <div className="box">
+        <h1>SignUp Form</h1>
+        <form role="form" method="post">
+          <div className="inputBox">
+            <input type="text" autoComplete="off" required />
+            <label>Firstname</label>
+          </div>
+          <div className="inputBox">
+            <input type="text" autoComplete="off" required />
+            <label>Lastname</label>
+          </div>
+          <div className="inputBox">
+            <input type="text" autoComplete="off" required />
+            <label>Username</label>
+          </div>
+          <div className="inputBox">
+            <input type="email" autoComplete="off" required />
+            <label>Email</label>
+          </div>
+          <div className="inputBox">
+            <input type="password" autoComplete="off" required />
+            <label>Password</label>
+          </div>
+
+          <input type="submit" className="register-btn" value="Sign Up" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default page;
