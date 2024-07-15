3e3c920a1f0768488dbecf2b9512e01d701811e4
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 18:21:13 +0330

    connect signin page with backend

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,8 @@ import { Provider } from "next-auth/providers/index";
 import credentialsProvider from "next-auth/providers/credentials";
 import { randomUUID } from "crypto";
 import { connectToDB } from "@/config/db";
+import { compereHashedPassword } from "@/utils/auth";
+import UserModel from "@/app/models/User";
 
 type Credentials = {
   isSignUp: true | undefined;
@@ -23,7 +25,24 @@ const providers: Provider[] = [
         return { id: randomUUID(), email: identifier };
       }
 
-      return null;
+      const user = await UserModel.findOne({
+        $or: [{ username: identifier }, { email: identifier }],
+      });
+
+      if (!user) {
+        throw new Error("Notfound user !!");
+      }
+
+      const isValidPassword = await compereHashedPassword(
+        password,
+        user.password
+      );
+
+      if (!isValidPassword) {
+        throw new Error("Username or password is not correct");
+      }
+
+      return { id: randomUUID(), email: user.email };
     },
   }),
 ];
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,21 +1,71 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
 function page() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const identifierRef = useRef<null | HTMLInputElement>(null);
+  const passwordRef = useRef<null | HTMLInputElement>(null);
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = {
+      identifier: identifierRef.current?.value || "",
+      password: passwordRef.current?.value || "",
+    };
+
+    startTransition(async () => {
+      const res = await signIn("credentials", {
+        redirect: false,
+        identifier: user.identifier,
+        password: user.password,
+      });
+
+      if (res?.status === 200) {
+        alert("welcome");
+        router.push("/dashboard");
+      } else {
+        alert("Not found user");
+      }
+    });
+  };
+
   return (
     <>
       <div className="box">
         <h1>Login Form</h1>
-        <form role="form" method="post">
+        <form role="form" method="post" onSubmit={submitForm}>
           <div className="inputBox">
-            <input type="text" autoComplete="off" required />
+            <input
+              disabled={isPending}
+              type="text"
+              autoComplete="off"
+              required
+              ref={identifierRef}
+            />
             <label>Username OR Email</label>
           </div>
           <div className="inputBox">
-            <input type="password" autoComplete="off" required />
+            <input
+              disabled={isPending}
+              type="password"
+              autoComplete="off"
+              required
+              ref={passwordRef}
+            />
             <label>Password</label>
           </div>
 
-          <input type="submit" className="register-btn" value="Sign In" />
+          <input
+            disabled={isPending}
+            type="submit"
+            className="register-btn"
+            value={isPending ? "Entering..." : "Sign In"}
+          />
         </form>
       </div>
     </>
