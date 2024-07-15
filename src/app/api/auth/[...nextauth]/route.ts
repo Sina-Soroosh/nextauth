ddc6491ce9fccc463e0fbf2cf1489dd89e7faf3d
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-07-15 18:08:31 +0330

    connect signup page with backend

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth/next";
 import { Provider } from "next-auth/providers/index";
 import credentialsProvider from "next-auth/providers/credentials";
+import { randomUUID } from "crypto";
+import { connectToDB } from "@/config/db";
+
+type Credentials = {
+  isSignUp: true | undefined;
+  identifier: string;
+  password: string;
+};
 
 const providers: Provider[] = [
   credentialsProvider({
     credentials: {},
     name: "Next Auth",
-    async authorize(credentials, req) {
-      console.log(credentials);
+    authorize: async (credentials, req) => {
+      await connectToDB();
+
+      const { isSignUp, identifier, password } = credentials as Credentials;
+
+      if (isSignUp) {
+        return { id: randomUUID(), email: identifier };
+      }
 
       return null;
     },
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,33 +1,129 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useTransition } from "react";
+import { User } from "../types/User.types";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function page() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const firstNameRef = useRef<null | HTMLInputElement>(null);
+  const lastNameRef = useRef<null | HTMLInputElement>(null);
+  const usernameRef = useRef<null | HTMLInputElement>(null);
+  const emailRef = useRef<null | HTMLInputElement>(null);
+  const passwordRef = useRef<null | HTMLInputElement>(null);
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newUser: User = {
+      firstName: firstNameRef.current?.value || "",
+      lastName: lastNameRef.current?.value || "",
+      username: usernameRef.current?.value || "",
+      email: emailRef.current?.value || "",
+      password: passwordRef.current?.value || "",
+    };
+
+    startTransition(async () => {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      switch (res.status) {
+        case 201: {
+          await signIn("credentials", {
+            redirect: false,
+            isSignUp: true,
+            identifier: newUser.email,
+          });
+
+          alert("register is successfully");
+          router.push("/dashboard");
+          break;
+        }
+        case 422: {
+          alert("Data is not valid !!");
+          break;
+        }
+        case 409: {
+          alert("This username or email is exist already !!");
+          break;
+        }
+
+        default:
+          alert("Error !!");
+          break;
+      }
+    });
+  };
+
   return (
     <>
       <div className="box">
         <h1>SignUp Form</h1>
-        <form role="form" method="post">
+        <form role="form" method="post" onSubmit={submitForm}>
           <div className="inputBox">
-            <input type="text" autoComplete="off" required />
-            <label>Firstname</label>
+            <input
+              disabled={isPending}
+              ref={firstNameRef}
+              type="text"
+              autoComplete="off"
+              required
+            />
+            <label>FirstName</label>
           </div>
           <div className="inputBox">
-            <input type="text" autoComplete="off" required />
-            <label>Lastname</label>
+            <input
+              disabled={isPending}
+              ref={lastNameRef}
+              type="text"
+              autoComplete="off"
+              required
+            />
+            <label>LastName</label>
           </div>
           <div className="inputBox">
-            <input type="text" autoComplete="off" required />
+            <input
+              disabled={isPending}
+              ref={usernameRef}
+              type="text"
+              autoComplete="off"
+              required
+            />
             <label>Username</label>
           </div>
           <div className="inputBox">
-            <input type="email" autoComplete="off" required />
+            <input
+              disabled={isPending}
+              ref={emailRef}
+              type="email"
+              autoComplete="off"
+              required
+            />
             <label>Email</label>
           </div>
           <div className="inputBox">
-            <input type="password" autoComplete="off" required />
+            <input
+              disabled={isPending}
+              ref={passwordRef}
+              type="password"
+              autoComplete="off"
+              required
+            />
             <label>Password</label>
           </div>
 
-          <input type="submit" className="register-btn" value="Sign Up" />
+          <input
+            disabled={isPending}
+            type="submit"
+            className="register-btn"
+            value={isPending ? "Registration..." : "Sign Up"}
+          />
         </form>
       </div>
     </>
